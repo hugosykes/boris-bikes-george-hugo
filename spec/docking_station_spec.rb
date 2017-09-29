6216986685fa90f368780b4261aa897a372f22e9
mormolis <george.lamprakis@gmail.com>
2017-09-29 16:47:07 +0100

    Completes Challenge 19

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,13 +1,14 @@
 require 'docking_station.rb'
-require 'bike'
+
+#require 'bike' #this needs to be removed
 
 describe DockingStation do
-
+  let(:bike) { double( :working? => true) }
+  let(:broken) {double( :working? => false)}
   describe "release bike tests" do
     it { is_expected.to respond_to :release_bike }
 
-    it "releases working bike" do
-      bike = Bike.new
+    it "releases working bike" do # reference to bike needs to be removed
       subject.dock(bike)
       expect(subject.release_bike).to respond_to(:working?)
     end
@@ -19,7 +20,7 @@ describe DockingStation do
     end
 
     it 'returns the docked bike' do
-      bike = Bike.new
+      #bike = Bike.new #refernece to bike needs to be removed
       subject.dock(bike)
       expect(subject.bikes.first).to eq bike
     end
@@ -27,9 +28,8 @@ describe DockingStation do
 
   describe "capacity tests" do
     it "should raise error when bikes are full" do
-      subject.capacity.times{ subject.dock(Bike.new) }
-      # the :: allows us to access the constant 'DEFAULT_CAPACITY' from the DockingStation class
-      expect {subject.dock(Bike.new)}.to raise_error("bikes full, no bike dock")
+      subject.capacity.times{ subject.dock(bike) }  #refernece to bike needs to be removed
+      expect {subject.dock(bike)}.to raise_error("bikes full, no bike dock") #refernece to bike needs to be removed
     end
 
     it 'should raise error when no bikes' do
@@ -38,14 +38,13 @@ describe DockingStation do
 
     it "shouldn't accept more bikes than we allow it to" do
       station = DockingStation.new(10)
-      expect { 11.times { station.dock(Bike.new) } }.to raise_error("bikes full, no bike dock")
+      expect { 11.times { station.dock(bike) } }.to raise_error("bikes full, no bike dock") #refernece to bike needs to be removed
     end
   end
 
   describe "broken bike tests" do
     it "should not release broken bikes" do
-      broken = Bike.new
-      broken.report_broken
+      #refernece to bike needs to be removed
       subject.dock(broken)
       expect{ subject.release_bike }.to raise_error("no bikes available")
     end
